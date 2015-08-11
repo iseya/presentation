51b10a480371ebc25ff281e1ef576c10c1a0bbaa
iseya <0610iseya@gmail.com>
2015-08-11 15:59:58 +0900

    homeにnew追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
  
   def show
   end
+
+  def new
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'home/show'
 
-   root to: "home#index"
+  get 'home/new'
+
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
