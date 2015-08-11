a6f6b866f2fd8f4eeb495d73864dbad6f46c0f1b
iseya <0610iseya@gmail.com>
2015-08-11 17:52:11 +0900

    post機能追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,25 @@
 class HomeController < ApplicationController
   def index
+  	@homes = Home.all
   end
  
   def show
+  	@home = Home.find(params[:id])
   end
 
   def new
   end
+
+  def create
+    @home = Home.new(home_params)
+    @home.save
+    redirect_to @home
+  end
+
+  private
+    def home_params
+      params.require(:home).permit(:title, :text)
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'home/index'
 
-  get 'home/show'
+  resources :home
 
-  get 'home/new'
 
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
